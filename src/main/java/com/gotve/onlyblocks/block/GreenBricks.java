9ce91f576f7ab0f5ebf73598bda46b2e2379bcbe
Sunrise <akselvolkov40@gmail.com>
2025-03-31 15:07:16 +0500

    1.2.0 added crossed block

--- src/main/java/com/gotve/onlyblocks/block/GreenBricks.java
+++ src/main/java/com/gotve/onlyblocks/block/GreenBricks.java
@@ -1,7 +1,9 @@
 package com.gotve.onlyblocks.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
 
 public class GreenBricks extends BlockBase{
 
@@ -13,6 +15,31 @@ public class GreenBricks extends BlockBase{
     public GreenBricks setCreativeTab(CreativeTabs tab) {
         super.setCreativeTab(tab);
         return this;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public boolean isTranslucent(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public float getAmbientOcclusionLightValue(IBlockState state) {
+        return 1.0F;
     }
 
 }
